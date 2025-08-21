a1326e7c14b13fe76bd0edd102cbf0484a0e2915
jcramossa <jcramossa@gmail.com>
2025-08-21 19:37:05 +0200

    Update remote_receiver_esp32.cpp

--- components/remote_receiver/remote_receiver_esp32.cpp
+++ components/remote_receiver/remote_receiver_esp32.cpp
@@ -2,7 +2,8 @@
 #include "esphome/core/log.h"
 
 #ifdef USE_ESP32
-#include <driver/rmt.h>
+#include <driver/rmt_tx.h>
+#include <driver/rmt_rx.h>
 
 namespace esphome {
 namespace remote_receiver {
